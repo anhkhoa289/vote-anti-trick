b55b1cf77cb28abc6b54d004f1fd8fdcb61d8f31
Khoa Storm <anhkhoa2890@gmail.com>
2025-11-26 00:03:52 +0700

    update image configure

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // https://nextjs.org/docs/messages/next-image-unconfigured-host
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**.wikipedia.org',
+      },
+      {
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.wikimedia.org',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
